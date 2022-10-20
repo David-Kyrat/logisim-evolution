337183a04785c00aac907557c55729e24204b4e0
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2022-10-20 21:30:07 +0200

    addded build dir with launcher

---
+++ build/generated/logisim/java/com/cburch/logisim/generated/BuildInfo.java
@@ -0,0 +1,35 @@
+// ************************************************************************
+// THIS IS A COMPILE TIME GENERATED FILE! DO NOT EDIT BY HAND!
+// Generated at 2022-10-20T20:58:52+0200
+// ************************************************************************
+
+package com.cburch.logisim.generated;
+
+import com.cburch.logisim.LogisimVersion;
+import java.util.Date;
+
+public final class BuildInfo {
+  // Build time VCS details
+  public static final String branchName = "test";
+  public static final String branchLastCommitHash = "1047be53";
+  public static final String buildId = "test/1047be53";
+
+  // Project build timestamp
+  public static final long millis = 1666292332293L; // keep trailing 'L'
+  public static final String year = "2022";
+  public static final String dateIso8601 = "2022-10-20T20:58:52+0200";
+  public static final Date date = new Date();
+  static { date.setTime(millis); }
+
+  // Project version
+  public static final LogisimVersion version = LogisimVersion.fromString("3.8.0");
+  public static final String name = "Logisim-evolution";
+  public static final String displayName = "Logisim-evolution v3.8.0";
+  public static final String url = "https://github.com/logisim-evolution/";
+
+  // JRE info
+  public static final String jvm_version =
+      String.format("%s v%s", System.getProperty("java.vm.name"), System.getProperty("java.version"));
+  public static final String jvm_vendor = System.getProperty("java.vendor");
+}
+// End of generated BuildInfo
